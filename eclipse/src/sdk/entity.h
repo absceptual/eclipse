f28a57288cd97217faca79a5d9d32ca5f1d852f8
absceptual <absceptual@gmail.com>
2022-11-16 20:37:08 -0600

    Revert "netvar manager added"
    This reverts commit 72aa0b183e4aac62349e0835b06571070949dec7.

--- eclipse/src/sdk/entity.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "netvars.h"
-
-class CEntity
-{
-public:
-	NETVAR(health, int, "DT_BasePlayer.m_iHealth")
-};
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-// C++ headers
-#include <stdio.h>
-
-// Windows and external headers
-#include <Windows.h>
-
-
